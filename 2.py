fc3ec59882ace393551afadf964f3be8e9709eb1
skgx <saurabhgupta2131@gmail.com>
2022-01-01 20:24:04 +0530

    video3

---
+++ 2.py
@@ -0,0 +1,14 @@
+print(3//2) #gives the floor division 
+print(abs(-4))
+
+print(round(2.46,1)) #to give number of digits in result
+print(round(2.46))
+
+num1='100'
+num2='200'
+
+num3=int(num1)
+num4=int(num2)
+
+print(num1+num2)
+print(num3+num4)
