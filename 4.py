6a9b04b773da138dab7dc25782183cf3541bf7b2
skgx <saurabhgupta2131@gmail.com>
2022-01-03 18:58:10 +0530

    init

---
+++ 4.py
@@ -0,0 +1,15 @@
+def hello_fun(greeting):
+    return'{} Function.'.format(greeting)
+print(hello_fun('hello'))
+
+def student_ingo(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+courses=['maths','history','science']
+info={'name':'saurabh','age':'20'}
+student_ingo(courses,info)
+
+student_ingo(*courses,**info)
+
+print("saurabh")
