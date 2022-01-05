e57e821cefc2bdf6de387105d5070001756d31a1
saurabh <saurabhgupta2131@gmail.com>
2022-01-05 11:34:22 +0530

    star

--- se-end.py
+++ se-end.py
@@ -1,4 +1,4 @@
-#User function Template for python3
+
 
 #Function using 'end' and 'sep' parameters to print desired output
 # string1 = "Geeks"
@@ -6,14 +6,18 @@
 def print_func(string1, string2):
     print (  string1,string2  ,string1  , sep = '$', end = '$')
 # Use string1 & string2 with sep and end parameter to print desired output
+#sep is separator for the given strings and end is the ending you want
+def print_fun1(string, x):
+    # Your code here
+    print(string*x) #prints the string desired number of times
 
-#{ 
-#Driver Code Starts.
 def main():
     string1 = "Geeks"
     string2 = "For"
     print_func(string1, string2)
     print()
+    print_fun1(string1,5)
+
 
 
 if __name__ == "__main__":
