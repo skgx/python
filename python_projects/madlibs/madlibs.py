6c12e7538239c0581dcb4f13eddbce39de80a519
saurabh <saurabhgupta2131@gmail.com>
2022-01-07 10:48:58 +0530

    created theory folder

---
+++ python_projects/madlibs/madlibs.py
@@ -0,0 +1,9 @@
+adj=input("Ajective:")
+verb1=input("verb1:")
+verb2=input("verb2:")
+famous_person=input("name:")
+
+madlib=f"Computer programming is so {adj}! It makes me feel so exited all the time \
+I love to {verb1}.Stay hydrated and {verb2} like you are {famous_person}"
+print(madlib)
+
---
+++ python_projects/madlibs/string_conat.py
@@ -0,0 +1,5 @@
+name="saurabh"
+
+print("hello "+name)
+print("hello {}".format(name))
+print(f"hello {name}")
