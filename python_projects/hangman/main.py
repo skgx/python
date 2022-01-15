688c041979f3b3691fd69535a436d661b3d3fc06
saurabh <saurabhgupta2131@gmail.com>
2022-01-15 23:25:41 +0530

    init

--- python_projects/hangman/main.py
+++ python_projects/hangman/main.py
@@ -4,8 +4,6 @@ import random
 from words import words
 from hangman_visual import lives_visual_dict
 import string
-
-
 def get_valid_word(words):
     word = random.choice(words)  # randomly chooses something from the list
     while '-' in word or ' ' in word:
