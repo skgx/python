c398ebab0135d314ef666a35019c38171d32c85e
skgx <saurabhgupta2131@gmail.com>
2022-07-18 19:41:59 +0530

    completed quiz game

--- python_projects/quizGame/quizGame.py
+++ python_projects/quizGame/quizGame.py
@@ -3,13 +3,40 @@ print("welcome to this quiz!")
 playing=input("Do you want to play? ")
 #print(playing)
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay ! let's play ")
+score=0
 
 answer=input("what does CPU stand for?")
-if answer == 'central processing unit':
+if answer.lower() == 'central processing unit':
     print("correct!")
+    score+=1
 else:
     print("ooops!wrong answer")
+
+answer=input("what does RAM stand for?")
+if answer.lower() == 'random access memory':
+    print("correct!")
+    score+=1
+else:
+    print("ooops!wrong answer")
+
+answer=input("what does GPU stand for?")
+if answer.lower() == 'graphics processing unit':
+    print("correct!")
+    score+=1
+else:
+    print("ooops!wrong answer")
+
+answer=input("what does PSU stand for?")
+if answer.lower() == 'power supply unit':
+    print("correct!")
+    score+=1
+else:
+    print("ooops!wrong answer")
+
+#here the num score is converted to string as a number can't be addeed directly to a string
+print("you got "+str(score)+" questions correct")
+print("you got "+str((score/4)*100)+"%")
