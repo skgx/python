00b97527936119d8200d5293a64b333310985fb3
skgx <saurabhgupta2131@gmail.com>
2022-09-24 15:00:35 +0530

    updated file structure

--- selenium/sel.py
+++ selenium/sel.py
@@ -1,1 +1,8 @@
-import selenium
+from selenium import webdriver
+PATH="C:\Program Files (x86)\chromedriver.exe"
+driver=webdriver.Chrome(PATH)
+
+driver.get("https://techwithtim.net")
+print(driver.title)
+driver.quit()
+
