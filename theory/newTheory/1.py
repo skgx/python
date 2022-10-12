926d440acc9913dd38c06b70c0e0cd442a055ed3
skgx <saurabhgupta2131@gmail.com>
2022-10-12 23:50:21 +0530

    day2

--- theory/newTheory/1.py
+++ theory/newTheory/1.py
@@ -2,3 +2,8 @@ def double(num):
     """Function to double the value"""
     return 2*num
 print(double.__doc__)
+
+#size of int->4 byte
+#size of float-> 8 bytes
+#size of boolean-> 1 byte
+#size of raw string-> 1 byte
